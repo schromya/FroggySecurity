7ff28abd7f96c979d201a226d4d10a5045d6945c
Mya Schroder <80082701+schromya@users.noreply.github.com>
2024-02-16 00:19:53 -0900

    Pushed froggy framework

---
+++ Frog.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+class Frog
+{
+
+public:
+    Frog();
+    void meet();
+    void feed();
+    void chat();
+};
+
+Frog::Frog()
+{
+    std::cout << "Welcome to Frog World!" << std::endl;
+
+    // TODO give options
+}
+
+void Frog::meet()
+{
+}
+
+void Frog::feed()
+{
+}
+
+void Frog::chat()
+{
+}
+
+int main()
+{
+    Frog myFrog;
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FroggySecurity
 Frog program to demonstrate security bugs. Team Buffer Busters.
+
+# Running
+To run this program on mac/linux, run `g++ Frog.cpp` and then `./a.out`.
