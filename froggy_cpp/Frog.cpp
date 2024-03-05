046c1720edeb32fd1d8dcf8b4b42e53b1a19bb15
AnthonyvW <akamvw@gmail.com>
2024-03-04 21:02:35 -0900

    updated Frog C++ with bouncy frog

--- froggy_cpp/Frog.cpp
+++ froggy_cpp/Frog.cpp
@@ -3,8 +3,10 @@
 #include <chrono>
 #include <thread>
 #include <Windows.h>
+#include <random>
 
-const int screenWidth = 100;
+
+const int screenWidth = 120;
 const int screenHeight = 20;
 unsigned int screenSize = screenWidth * screenHeight;
 std::string screen(screenSize, '*');
@@ -18,7 +20,7 @@ struct sprite {
 void copySprite(sprite image, unsigned int xPos, unsigned int yPos) {
     for (int y = 0; y < image.height; y++) {
         for (int x = 0; x < image.width; x++) {
-            if (!(yPos + y < 0 || yPos + y >= screenHeight || xPos + x < 0 || xPos + x >= screenWidth)) {
+            if (image.values.size() > x + y * image.width && image.values[x + y * image.width] != ' ' && !(yPos + y < 0 || yPos + y >= screenHeight || xPos + x < 0 || xPos + x >= screenWidth)) {
                 screen[x + xPos + (y+yPos) * screenWidth] = image.values[x + y * image.width];
             }
         }
@@ -28,7 +30,7 @@ void copySprite(sprite image, unsigned int xPos, unsigned int yPos) {
 void resetScreen() {
     for (int y = 0; y < screenHeight; y++) {
         for (int x = 0; x < screenWidth; x++) {
-            screen[x + y * screenWidth] = '*';
+            screen[x + y * screenWidth] = ' ';
         }
     }
 }
@@ -52,9 +54,19 @@ void drawScreen() {
     std::cout << std::flush;
 }
 
+void updateScore(sprite * image, int score) {
+    image->values = R"(Score : )" + std::to_string(score);
+}
 
 int main()
 {
+    // Hide the Cursor
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100; // The size of the cursor
+    info.bVisible = FALSE; // Set the cursor visibility to FALSE
+    SetConsoleCursorInfo(consoleHandle, &info);
+
     sprite frogsit;
     frogsit.width = 15;
     frogsit.height = 4;
@@ -77,29 +89,67 @@ int main()
 
     std::string name;
     std::cout << "What is your name? Max 10 letters" << std::endl;
-    //std::getline(std::cin, name);
+    std::getline(std::cin, name);
 
+    /*
+    if (name.size() < 10) {
+        for (int i = 0; i < 10 - name.size(); i++) {
+            name += " ";
+        }
+    } else if (name.size() > 10) {
+
+    }
+    */
     sprite nametag;
     nametag.width = 12;
     nametag.height = 3;
     nametag.values = R"(============|John Cena |============)";
     
+    sprite score;
+    int currentScore = 0;
+    score.width = 12;
+    score.height = 1;
+    //score.values = R"(Score : 0)";
+
     int mushroomPos = 0;
+    int frogPos = 0;
+    bool frogHasWings = true;
+    bool isTall = std::rand() % 2;
     while (true) {
-        // Hide the Cursor
-        HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-        CONSOLE_CURSOR_INFO info;
-        info.dwSize = 100; // The size of the cursor
-        info.bVisible = FALSE; // Set the cursor visibility to FALSE
-        SetConsoleCursorInfo(consoleHandle, &info);
+
+        updateScore(&score, currentScore);
+        if (mushroomPos > screenWidth + mushroomShort.width) {
+            mushroomPos = 0;
+            isTall = std::rand() % 2;
+        }
+        if (frogHasWings && frogPos > -10) {
+            frogPos--;
+        } else {
+            if (frogPos < 0) {
+                frogHasWings = false;
+                frogPos++;
+            } else {
+                frogHasWings = true;
+            }
+        }
 
         resetScreen();
-        copySprite(frogsit, 15, 16);
-        copySprite(mushroomShort, 100 - mushroomShort.width - mushroomPos, 16);
-        copySprite(nametag, 0, 0);
+        if (frogHasWings) {
+            copySprite(frogjump, 20, 16 + frogPos);
+        } else {
+            copySprite(frogsit, 20, 16 + frogPos);
+        }
+        if (isTall) {
+            copySprite(mushroomTall , screenWidth - mushroomShort.width - mushroomPos, 15);
+        } else {
+            copySprite(mushroomShort, screenWidth - mushroomShort.width - mushroomPos, 16);
+        }
+        copySprite(nametag, 3, 1);
+        copySprite(score, screenWidth - score.width - 5, 2);
         drawScreen();
         mushroomPos++;
         mushroomPos++;
+        currentScore++;
         //std::this_thread::sleep_for(std::chrono::milliseconds(15));
     }
 }
